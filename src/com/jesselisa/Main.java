bf01b7cdd7dfd056cf0d01049afda3bc2bdd266f
LisaMabley <lisamabley@gmail.com>
2015-02-17 20:22:17 -0600

    Added code to create deck of card objects.

--- src/com/jesselisa/Main.java
+++ src/com/jesselisa/Main.java
@@ -1,9 +1,19 @@
 package com.jesselisa;
+import Java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        
+    public static void main() {
 
+        for (int x = 1; x < 4; x++) {
+            int suit = x;
+            int value;
+
+            for (int x = 1; x < 14; x ++) {
+                value = x;
+            }
+            Card newcard = new Card (suit, value);
+            System.out.println(newcard);
+        }
     }
 }
